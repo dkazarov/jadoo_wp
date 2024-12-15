600420c5af37a8278a6797961cecaef6eb150d54
unknown <d.kazarov1361@gmail.com>
2024-12-15 14:40:33 +0200

    header: change structure scss folders + add show|hide nav

--- src/js/js-components/burger.ts
+++ src/js/js-components/burger.ts
@@ -1,10 +1,12 @@
 const burger = document.querySelector('.burger');
 const burgerLine = document.querySelector('.burger__line');
+const nav = document.querySelector('nav');
 
 export const burgerOpen = (): void => {
 	burger.addEventListener('click', () => {
 		burgerLine.classList.toggle('line--hide');
 		burger.classList.toggle('burger--transform');
+		nav.classList.toggle('show-nav');
 	});
 };
 
